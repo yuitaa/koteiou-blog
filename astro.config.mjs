4f4cc55015d74ab8750aa258f1a151f3dfda28be
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-21 16:36:42 +0900

    mdx対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()]
 });
