1610df903516516cea3e465d265b9047d4582a0b
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-22 00:02:56 +0900

    サイトurlを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://koteiou.pages.dev/',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [
