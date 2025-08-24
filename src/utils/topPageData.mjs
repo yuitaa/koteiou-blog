c8ea1e601ed1b3a7492cd91a4343b62d2dd69ff2
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-25 00:10:05 +0900

    トップページを更新

---
+++ src/utils/topPageData.mjs
@@ -0,0 +1,70 @@
+export const links = [
+  {
+    icon: '🐦',
+    name: 'Twitter / X',
+    url: 'https://twitter.com/koteiou',
+    handle: '@koteiou',
+  },
+  {
+    icon: '🎥',
+    name: 'Twitch',
+    url: 'https://www.twitch.tv/yuitaaaaa',
+    handle: 'yuitaaaaa',
+  },
+  {
+    icon: '📼',
+    name: 'YouTube',
+    url: 'https://www.youtube.com/@yuita',
+    handle: '@yuita',
+  },
+  { icon: '📖', name: 'note', url: 'https://note.com/yuita', handle: 'yuita' },
+  { icon: '💬', name: 'Discord', handle: 'yu.it.a' },
+  {
+    icon: '🏆',
+    name: 'speedrun.com',
+    url: 'https://www.speedrun.com/ja-JP/users/yuita_',
+    handle: 'yuita_',
+  },
+  {
+    icon: '🎮',
+    name: 'Steam',
+    url: 'https://steamcommunity.com/profiles/76561199572949580/',
+    handle: '1612683852',
+  },
+  {
+    icon: '🐈️',
+    name: 'GitHub',
+    url: 'https://github.com/yuitaa/',
+    handle: 'yuitaa',
+  },
+  { icon: '🀄', name: '雀魂', handle: '89555910' },
+  {
+    icon: '🚂',
+    name: 'Unrailed 2',
+    url: 'https://u2.unrailed-online.com/#/userHighscore/c0af36b9-d862-4e4c-b3d4-b160fa599af7',
+    handle: 'c0af36b9-d862-4e4c-b3d4-b160fa599af7',
+  },
+];
+
+export const creations = [
+  { name: 'koteiou.pages.dev (このサイト!)', url: null },
+  {
+    name: 'U.L.T.R.A.大会ウェブサイト',
+    url: 'https://ultra-u2.vercel.app/ultra/',
+  },
+  {
+    name: 'Unrailed 2 パッチノート検索',
+    url: 'https://u2-patch-search.vercel.app/',
+  },
+  { name: 'u2-wagon', url: 'https://github.com/yuitaa/u2-wagon' },
+  {
+    name: '@UnrailedScore (Twitter Bot, 停止)',
+    url: 'https://x.com/UnrailedScore',
+  },
+  {
+    name: 'Unrailed! Highscores (Discordサーバー)',
+    url: 'https://discord.gg/62AFewzRNU',
+  },
+  { name: 'Unrailed! 記録掲示板 (U1日本鯖)', url: null },
+  { name: 'Kabosu Items', url: null },
+];
