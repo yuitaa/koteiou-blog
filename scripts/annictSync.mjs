0c67f725b631454a23b7cf8223aadaededea72e4
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-25 15:58:21 +0900

    ファイル名を小文字に

--- scripts/annictSync.mjs
+++ scripts/annictSync.mjs
@@ -16,7 +16,7 @@ function getWeekNumber(date) {
   d.setUTCDate(d.getUTCDate() + 4 - dayNum);
   const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
   const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  return `${d.getUTCFullYear()}W${String(weekNo).padStart(2, '0')}`;
+  return `${d.getUTCFullYear()}w${String(weekNo).padStart(2, '0')}`;
 }
 
 async function sync() {
