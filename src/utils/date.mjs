2ddef8aad343d2c33c11767377e2c10a712704cd
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-25 19:52:17 +0900

    とりあえず視聴記録ページを実装

--- src/utils/date.mjs
+++ src/utils/date.mjs
@@ -8,3 +8,30 @@ export function getDayOfWeek(date) {
 
   return (((jstDate.getUTCDay() - 1) % 7) + 7) % 7;
 }
+
+/**
+ * 指定された年の週の最初の日の日付を返します。
+ * @param {string} yearWeek "YYYYwWW"
+ * @returns {number[]} [年, 月, 日]
+ */
+export function getFirstDayOfWeek(yearWeek) {
+  const [year, week] = yearWeek
+    .match(/(\d{4})w(\d{2})/)
+    .slice(1)
+    .map(Number);
+  const date = new Date(year, 0, 1 + (week - 1) * 7);
+  const day = date.getDay();
+  const diff = day === 0 ? 6 : day - 1;
+  date.setDate(date.getDate() - diff);
+  return [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+}
+
+export function padZero(number) {
+  return String(number).padStart(2, '0');
+}
+
+export function getDisplayDate(weekFirstDay, day) {
+  const date = new Date(weekFirstDay[0], weekFirstDay[1] - 1, weekFirstDay[2]);
+  date.setDate(date.getDate() + day);
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+}
