bcba22b1461a636773cbe953e22eab3264bb0752
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-08-26 14:31:44 +0900

    rssを作成

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'koteiou.pages.dev',
+    description: 'yuitaのブログ',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description:
+        post.body.length > 1000 ? post.body.slice(0, 1000) + '...' : post.body,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
